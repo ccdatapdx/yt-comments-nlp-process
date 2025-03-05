2160ce5bbbccbaa6cc5923b5cbe1666a7b7a5084
ccdatapdx <caseycushing@gmail.com>
2025-03-05 10:18:47 -0800

    realized that previous model made image too large for lambda, so the function wasn't being updated and also therefore the data wasn't being replaced

--- lambda_function.py
+++ lambda_function.py
@@ -4,5 +4,5 @@ from yt_channel_process_nlp import SpacyProcessing
 def lambda_handler(event,context):
      
      event = SpacyProcessing()
-     event = event.process_spacy_ner()
+     event = event.process_spacy()
      return event
